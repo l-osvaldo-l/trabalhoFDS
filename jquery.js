07bb6f1988a5e255bf7abbc8864ca03c36844b46
Osvaldo <osvaldojoseda.silva2004@gmail.com>
2025-07-20 23:59:12 -0300

    Revert "Update jquery.js"
    This reverts commit 984b9acce8ad42db9888a84b67e2e4a8a6550fe5.

--- jquery.js
+++ jquery.js
@@ -1,13 +1,11 @@
-const botão = document.querySelector('.botao'); 
-let saudação = document.querySelector('.saudacao'); 
- 
-botão.addEventListener("click", saudar); 
- 
-function saudar() { 
-    saudação.innerHTML = "Olá, seja bem vindo ao site do meu trabalho da facul
-dade! Aproveite seu tempo aqui para aprender um pouco mais sobre mim e não he
-site em procurar por easter eggs!"; 
-        setTimeout(() => { 
-            saudação.innerHTML = ""; 
-        }, 5000); 
+const botão = document.querySelector('.botao');
+let saudação = document.querySelector('.saudacao');
+
+botão.addEventListener("click", saudar);
+
+function saudar() {
+    saudação.innerHTML = "Olá, seja bem vindo ao site do meu trabalho da faculdade! Aproveite seu tempo aqui para aprender um pouco mais sobre mim e não hesite em procurar por easter eggs!";
+        setTimeout(() => {
+            saudação.innerHTML = "";
+        }, 5000);
 }
