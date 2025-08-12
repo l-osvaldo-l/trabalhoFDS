88760483d46ca4a8f81397dd09a263fbf699fe7b
Osvaldo <osvaldojoseda.silva2004@gmail.com>
2025-08-12 16:13:11 -0300

    Update jquery.js
    Corrigindo hifenização do word

--- jquery.js
+++ jquery.js
@@ -4,7 +4,7 @@ let saudação = document.querySelector('.saudacao');
 botão.addEventListener("click", saudar);
 
 function saudar() {
-    saudação.innerHTML = "Olá, seja bem vindo ao site do meu trabalho da faculda-de! Aproveite seu tempo aqui para aprender um pouco mais sobre mim e não hesite em procurar por easter eggs!";
+    saudação.innerHTML = "Olá, seja bem vindo ao site do meu trabalho da faculdade! Aproveite seu tempo aqui para aprender um pouco mais sobre mim e não hesite em procurar por easter eggs!";
         setTimeout(() => {
             saudação.innerHTML = "";
         }, 5000);
